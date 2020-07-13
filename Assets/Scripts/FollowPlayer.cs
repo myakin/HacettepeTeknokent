e7225862554b78defe9906d2a2675aff9bd8c217
Murat Yakin <yakin_murat@yahoo.com>
2020-07-14 00:40:04 +0300

    Terrain, basit karakter, texturelama icin temel setup

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+    public Transform player;
+    
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = new Vector3 (player.transform.position.x, transform.position.y, player.transform.position.z);
+    }
+}
